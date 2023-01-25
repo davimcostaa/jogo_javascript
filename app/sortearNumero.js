8d271e7d27ec45d8a49bbdb6badb03ab035f37ef
Davi Costa <davim.costa@outlook.com>
2023-01-25 17:52:44 -0300

    first commit

---
+++ app/sortearNumero.js
@@ -0,0 +1,16 @@
+const menorValor = 1
+const maiorValor = 1000
+const numeroSecreto = gerarNumeroAleatorio()
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1)
+}
+
+console.log(numeroSecreto)
+
+const numeroMenor = document.getElementById('menor-valor')
+numeroMenor.innerHTML = menorValor
+
+const numeroMaior = document.getElementById('maior-valor')
+numeroMaior.innerHTML = maiorValor
+
